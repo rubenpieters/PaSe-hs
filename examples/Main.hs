0d401afad88248275cc11dcffda2bb7c90e9ce86
rubenpieters <rubenpietersgithub@gmail.com>
2020-02-04 10:44:12 +0100

    add bg with parallax

--- examples/Main.hs
+++ examples/Main.hs
@@ -78,6 +78,20 @@ instance Monoid m => Create s (Const m) where
 instance Monoid m => Delete s (Const m) where
   delete _ = Const mempty
 
+data BgCounters = BgCounters
+  { _moving :: Bool
+  , _counter1 :: Float
+  , _flipped1 :: Bool
+  , _counter2 :: Float
+  , _flipped2 :: Bool
+  , _counter3 :: Float
+  , _flipped3 :: Bool
+  , _counter4 :: Float
+  , _flipped4 :: Bool
+  }
+
+makeLenses ''BgCounters
+
 data GameView = GameView
   { _vPlayer :: (Float, Float, String, Bool)
   , _vSlime :: (Float, Float, String)
@@ -85,6 +99,7 @@ data GameView = GameView
   , _currentParticleId :: Int
   , _vBackground :: ()
   , _vMenu :: (Float, Float, Float, Bool)
+  , _bgCounters :: BgCounters
   }
 
 makeLenses ''GameView
@@ -107,6 +122,17 @@ initialView = GameView
   , _currentParticleId = 0
   , _vBackground = ()
   , _vMenu = (100, 80, 0, False)
+  , _bgCounters = BgCounters
+    { _moving = False
+    , _counter1 = 0
+    , _flipped1 = False
+    , _counter2 = 0
+    , _flipped2 = False
+    , _counter3 = 0
+    , _flipped3 = False
+    , _counter4 = 0
+    , _flipped4 = False
+    }
   }
 
 initialAnims :: GameAnims
@@ -114,7 +140,7 @@ initialAnims = GameAnims
   { _aPlayer = Nothing
   , _aSlime = Nothing
   , _aParticles = Nothing
-  , _aBackground = Nothing
+  , _aBackground = Just (scrollingBg 5 (bgCounters . counter1) (bgCounters . flipped1) `parallel` scrollingBg 7.5 (bgCounters . counter2) (bgCounters . flipped2) `parallel` scrollingBg 10 (bgCounters . counter3) (bgCounters . flipped3) `parallel` scrollingBg 15 (bgCounters . counter4) (bgCounters . flipped4))
   , _aMenu = Just menuIntro
   }
 
@@ -139,7 +165,7 @@ textureFromMap textures str = case Map.lookup str textures of
   Nothing -> error ("unknown texture: " ++ str)
 
 textureNames :: [String]
-textureNames = nub (getConst (moveAnim 0) ++ ["playerAtk2_0.png", "playerAtk2_1.png", "playerAtk2_2.png", "playerAtk2_3.png", "playerAtk2_4.png", "playerAtk2_5.png", "minusOne.png", "menu.jpg", "slimeHurt0.png", "slimeHurt1.png", "slimeHurt2.png", "slimeHurt3.png", "slimeIdle.png", "bgLayer0.png", "bgLayer1.png"])
+textureNames = nub (getConst (moveAnim 0) ++ ["playerAtk2_0.png", "playerAtk2_1.png", "playerAtk2_2.png", "playerAtk2_3.png", "playerAtk2_4.png", "playerAtk2_5.png", "minusOne.png", "menu.jpg", "slimeHurt0.png", "slimeHurt1.png", "slimeHurt2.png", "slimeHurt3.png", "slimeIdle.png", "bgLayer0.png", "bgLayer1.png", "bgLayer2.png", "bgLayer3.png", "bgLayer4.png", "bgLayer5.png", "bgLayer6.png", "bgLayer7.png", "bgLayer8.png", "bgLayer9.png"])
 
 -- ++ getConst attackAnim ++ ["menu.jpg"] ++ getConst slimeHurt)
 
@@ -165,6 +191,20 @@ main = do
   SDL.destroyRenderer renderer
   SDL.destroyWindow window
   SDL.quit
+
+scrollingBg :: (Monad f, LinearTo GameView f, Set GameView f, Get GameView f) =>
+  Float -> Lens' GameView Float -> Lens' GameView Bool -> f ()
+scrollingBg dur counter flipped =
+  linearTo counter (For dur) (To (-928))
+  `sequential`
+  set counter 0
+  `sequential`
+  ( do
+    x <- get flipped
+    set flipped (not x)
+    )
+  `sequential`
+  scrollingBg dur counter flipped
 
 menuIntro :: (Applicative f, Parallel f, LinearTo GameView f, Set GameView f) =>
   f ()
@@ -217,7 +257,7 @@ hitStop :: (Functor f, IfThenElse f, Delay f, Get GameView f) =>
 hitStop =
   ifThenElse
     (fmap (\loc -> loc == 300) (get (vPlayer . _1)))
-    (delay 0.2)
+    (delay 0.15)
     (delay 0.05)
 
 slimeHurtSheet :: (Applicative f, Delay f, SetTexture GameView f, Get GameView f, IfThenElse f) =>
@@ -261,40 +301,68 @@ appLoop renderer textures view anims Counter{ now, prev } = do
       dPressed = any (eventIsPress SDL.KeycodeD) events
       aPressed = any (eventIsPress SDL.KeycodeA) events
       mPressed = any (eventIsPress SDL.KeycodeM) events
+      ePressed = any (eventIsPress SDL.KeycodeE) events
       spcPressed = any (eventIsPress SDL.KeycodeSpace) events
   -- update game view
   let view1 = case (aPressed, dPressed) of
         (True, _) -> view & vPlayer . _4 .~ True
         (_, True) -> view & vPlayer . _4 .~ False
         (_, _) -> view
+  let view2 = case (ePressed) of
+        True -> view1 & bgCounters . moving %~ not
+        _ -> view1
+  let viewU = view2
   -- update animations
   let anims1 = case (aPressed, dPressed, spcPressed) of
-        (True, _, _) -> anims & aPlayer %~ \l -> addIfNothing l (moveAnim ((view1 ^. vPlayer . _1) - 50))
-        (_, True, _) -> anims & aPlayer %~ \l -> addIfNothing l (moveAnim ((view1 ^. vPlayer . _1) + 50))
+        (True, _, _) -> anims & aPlayer %~ \l -> addIfNothing l (moveAnim ((viewU ^. vPlayer . _1) - 50))
+        (_, True, _) -> anims & aPlayer %~ \l -> addIfNothing l (moveAnim ((viewU ^. vPlayer . _1) + 50))
         (_, _, True) -> anims & aPlayer %~ \l -> addIfNothing l attackAnim
         (_, _, _) -> anims
-  let anims2 = case (mPressed, view1 ^. vMenu . _4) of
+  let anims2 = case (mPressed, viewU ^. vMenu . _4) of
         (True, False) -> anims1 & aMenu %~ \l -> addIfNothing l menuIntro
         (True, True) -> anims1 & aMenu %~ \l -> addIfNothing l menuOutro
         (False, _) -> anims1
   -- update game with animations
-  let (view2, anims3) = runAllAnimations deltaTime (view1, anims2)
+  let (viewA, animsA) = runAllAnimations deltaTime (viewU, anims2)
   -- draw game
-  drawGame renderer textures view1
+  drawGame renderer textures viewU
   -- loop or quit
-  unless qPressed (appLoop renderer textures view2 anims3 counter')
+  unless qPressed (appLoop renderer textures viewA animsA counter')
 
 drawGame :: SDL.Renderer -> Textures -> GameView -> IO ()
 drawGame renderer textures view = do
   SDL.rendererDrawColor renderer $= V4 0 0 0 0
   SDL.clear renderer
+  -- draw background
+  let (bgXOffset4_A, bgXOffset4_B) = if view ^. bgCounters . flipped3
+                     then (view ^. bgCounters . counter4 + 928, view ^. bgCounters . counter4)
+                     else (view ^. bgCounters . counter4, view ^. bgCounters . counter4 + 928)
+  for_ (reverse [8 .. 9]) $ \i -> do
+    drawTexture (bgXOffset4_A) (-293) (928) (793) Nothing Nothing ("bgLayer" ++ show i ++ ".png") renderer textures
+    drawTexture (bgXOffset4_B) (-293) (928) (793) Nothing Nothing ("bgLayer" ++ show i ++ ".png") renderer textures
+  let (bgXOffset3_A, bgXOffset3_B) = if view ^. bgCounters . flipped2
+                     then (view ^. bgCounters . counter3 + 928, view ^. bgCounters . counter3)
+                     else (view ^. bgCounters . counter3, view ^. bgCounters . counter3 + 928)
+  for_ (reverse [4 .. 7]) $ \i -> do
+    drawTexture (bgXOffset3_A) (-293) (928) (793) Nothing Nothing ("bgLayer" ++ show i ++ ".png") renderer textures
+    drawTexture (bgXOffset3_B) (-293) (928) (793) Nothing Nothing ("bgLayer" ++ show i ++ ".png") renderer textures
+  let (bgXOffset2_A, bgXOffset2_B) = if view ^. bgCounters . flipped2
+                     then (view ^. bgCounters . counter2 + 928, view ^. bgCounters . counter2)
+                     else (view ^. bgCounters . counter2, view ^. bgCounters . counter2 + 928)
+  for_ (reverse [2 .. 3]) $ \i -> do
+    drawTexture (bgXOffset2_A) (-293) (928) (793) Nothing Nothing ("bgLayer" ++ show i ++ ".png") renderer textures
+    drawTexture (bgXOffset2_B) (-293) (928) (793) Nothing Nothing ("bgLayer" ++ show i ++ ".png") renderer textures
   -- draw enemy
   drawTexture (view ^. vSlime . _1) (view ^. vSlime . _2) (30*2) (25*2) Nothing Nothing (view ^. vSlime . _3) renderer textures
   -- draw player
   drawTexture (view ^. vPlayer . _1) (view ^. vPlayer . _2) (50*2) (37*2) Nothing (Just (view ^. vPlayer . _4)) (view ^. vPlayer . _3) renderer textures
   -- draw foreground
-  drawTexture (0) (-293) (928) (793) Nothing Nothing ("bgLayer1.png") renderer textures
-  drawTexture (0) (-293) (928) (793) Nothing Nothing ("bgLayer0.png") renderer textures
+  let (bgXOffset1_A, bgXOffset1_B) = if view ^. bgCounters . flipped1
+                   then (view ^. bgCounters . counter1 + 928, view ^. bgCounters . counter1)
+                   else (view ^. bgCounters . counter1, view ^. bgCounters . counter1 + 928)
+  for_ (reverse [0 .. 1]) $ \i -> do
+    drawTexture (bgXOffset1_A) (-293) (928) (793) Nothing Nothing ("bgLayer" ++ show i ++ ".png") renderer textures
+    drawTexture (bgXOffset1_B) (-293) (928) (793) Nothing Nothing ("bgLayer" ++ show i ++ ".png") renderer textures
   -- draw particles
   for_ (view ^. vParticles) $ \particle ->
     drawTexture (particle ^. _1) (particle ^. _2) (13*2) (16*2) (Just (particle ^. _5)) Nothing (particle ^. _3) renderer textures
@@ -321,6 +389,10 @@ runAllAnimations :: Float -> (GameView, GameAnims) -> (GameView, GameAnims)
 runAllAnimations delta = S.execState $ do
   S.modify (runAnimations aPlayer delta)
   S.modify (runAnimations aMenu delta)
+  (view, _) <- S.get
+  if view ^. bgCounters . moving
+    then S.modify (runAnimations aBackground delta)
+    else return ()
 
 runAnimations :: Lens' GameAnims (Maybe (Animation GameView Identity ())) -> Float -> (GameView, GameAnims) -> (GameView, GameAnims)
 runAnimations lens delta (view, anims) =
