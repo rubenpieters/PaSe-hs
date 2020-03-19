221cbea8a09d1899ade635f0ba4d7e69ec25d952
rubenpieters <rubenpietersgithub@gmail.com>
2020-03-19 15:59:38 +0100

    calc frame rate from draw calls, not update calls

--- PaSe-benchmarks/Benchmark/Main.hs
+++ PaSe-benchmarks/Benchmark/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -10,14 +11,17 @@ import PaSe
 
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as IntMap
+import Data.IORef
 
 import Graphics.Gloss
-import Graphics.Gloss.Interface.Pure.Game
+import Graphics.Gloss.Interface.IO.Game
 
 import Lens.Micro hiding (set)
 import Lens.Micro.TH
 
 import Control.Monad.Identity
+
+import System.Clock
 
 type Dot = (Float, Float, Float, Int)
 
@@ -35,8 +39,8 @@ data Application = Application
 
 makeLenses ''Application
 
-initialApplication :: Application
-initialApplication = Application
+initialApplication :: TimeSpec -> Application
+initialApplication t = Application
   { _animations = mainAnimation
   , _appState = AppState
     { _dots = IntMap.empty
@@ -44,20 +48,32 @@ initialApplication = Application
     }
   }
 
-update :: Float -> Application -> Application
+update :: Float -> Application -> IO Application
 update t app = let
   (appState', animResult, _) = runIdentity $ runAnimation (app ^. animations) (app ^. appState) t
-  in case animResult of
-    Left anims' -> app & appState .~ appState'
-                       & animations .~ anims'
-    Right _ -> app & appState .~ appState'
-                   & animations .~ return ()
+  in return $ app & appState .~ appState'
+               & animations .~ case animResult of
+                                      Left anims' -> anims'
+                                      Right _ -> return ()
 
 handleInput :: Event -> Application -> Application
 handleInput e app = app
 
-draw :: Application -> Picture
-draw app = Pictures (map drawDot (IntMap.elems (app ^. appState . dots)))
+draw :: IORef TimeSpec -> IORef [Double] -> Application -> IO Picture
+draw prevTimeRef diffsRef app = do
+  -- read data
+  prevTime <- readIORef prevTimeRef
+  diffs <- readIORef diffsRef
+  newTime <- getTime Monotonic
+  -- calc diff
+  let diff = ((fromIntegral (toNanoSecs newTime) - fromIntegral (toNanoSecs prevTime)) :: Double) / 1000000000
+      newDiffs = addQueue 30 diff diffs
+  -- write refs
+  writeIORef prevTimeRef newTime
+  writeIORef diffsRef newDiffs
+  return $ Pictures $
+    map drawDot (IntMap.elems (app ^. appState . dots)) ++
+    [text ("FPS: " ++ show (fps newDiffs)) & color red & scale 0.2 0.2 & translate (-200) (200)]
 
 drawDot :: Dot -> Picture
 drawDot (x, y, alpha, _) = circleSolid 2 & Translate x y & Color (withAlpha alpha white)
@@ -68,7 +84,11 @@ main = let
   sw = 500
   sh = 500
   window = InWindow "benchmarks" (sw, sh) (100, 100)
-  in play window bgColor 60 initialApplication draw handleInput update
+  in do
+    newTime <- getTime Monotonic
+    prevTimeRef <- newIORef newTime
+    diffsRef <- newIORef ([] :: [Double])
+    playIO window bgColor 60 (initialApplication newTime) (draw prevTimeRef diffsRef) (\e s -> return (handleInput e s)) update
 
 createDotAnim :: (Monad f, Parallel f, LinearTo AppState f, Dots AppState f)
   => f ()
@@ -87,7 +107,7 @@ mainAnimation = do
   createDotAnim
   `parallel`
   ( do
-      delay 0.001
+      delay (1 / 400)
       mainAnimation
   )
 
@@ -122,3 +142,11 @@ instance (Monad m) => Dots s (Animation s m) where
     s' = f i s
     in return (s', Right (), Just delta)
 
+addQueue :: Int -> a -> [a] -> [a]
+addQueue i a l = take i (a:l)
+
+avg :: (Fractional a) => [a] -> a
+avg xs = sum xs / fromIntegral (length xs)
+
+fps :: [Double] -> Int
+fps diffs = floor (1 / avg diffs)
